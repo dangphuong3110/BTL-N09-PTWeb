2a1fa573a42b2e9cbb73c6c36fc057e550c7d989
Nguyễn Đăng Phương <104370972+dangphuong3110@users.noreply.github.com>
2022-06-10 19:09:52 +0700

    thêm navbar cho CV.html

---
+++ Assets/Guests/js/CV.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(window).bind('scroll', function(){
+        var gap = 400;
+        if($(window).scrollTop() > gap){
+            $('nav').removeClass('navbar-transparent');
+        }
+        else{
+            $('nav').addClass('navbar-transparent');
+        }
+    })
+})
