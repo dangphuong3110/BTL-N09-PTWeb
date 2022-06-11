225dbeaedf004dae3a4e574f191784b86b31bc83
QThai1704 <quangthai170402@gmail.com>
2022-06-11 21:42:53 +0700

    thêm thanh navbar

---
+++ Assets/Guests/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    //slider main
+    $('#slider-main').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+
+            },
+            600: {
+
+            },
+            1000: {
+
+            }
+        }
+    })
+});
