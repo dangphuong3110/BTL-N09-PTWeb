5e576abd70ced479f16d7d31c24d00d0de3da292
Nguyễn Đăng Phương <104370972+dangphuong3110@users.noreply.github.com>
2022-06-14 12:43:00 +0700

    thêm responsive cho img ở porfolio

--- Assets/Guests/js/CV.js
+++ Assets/Guests/js/CV.js
@@ -19,11 +19,9 @@ for(let i=0; i<navLink.length; i++){
     navLink[i].addEventListener('click', function(){
         for(let j=0; j<navLink.length; j++){
             navLink[j].classList.remove('active');
-            navLink[j].setAttribute('aria-expanded', 'false');
             tabPane[j].classList.remove('active');
         }
         navLink[i].classList.add('active');
-        navLink[i].setAttribute('aria-expanded', 'true');
         tabPane[i].classList.add('active');
     });
 };
