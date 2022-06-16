52f480b36dbade0570e99b82a8b02e2729351571
QThai1704 <quangthai170402@gmail.com>
2022-06-16 11:27:27 +0700

    xử lý sự kiện và biểu thức chính quy của input username và password

--- Assets/Admin/js/login.js
+++ Assets/Admin/js/login.js
@@ -27,13 +27,22 @@ login__back.addEventListener("click", function () {
 // })
 
 var user = document.getElementById('userName');
+var pass = document.getElementById('passWord');
 var note__user = document.getElementById('note__user');
 var note__pass = document.getElementById('note__pass');
 
+
+//Xử lý sự kiện và biểu thức chính quy của input userName
 function checkName(userName) {
     var check = /^[A-Za-z0-9]{8,20}$/
     return check.test(userName);
 }
+user.addEventListener("blur", function () {
+    if (user.value.length === 0) {
+        note__user.textContent = "Must enter input";
+
+    }
+})
 
 user.addEventListener("change", function () {
     if (checkName(user.value)) {
@@ -42,5 +51,26 @@ user.addEventListener("change", function () {
     else {
         note__user.textContent = "Must be alphabetic or numeric and length 8-20";
         note__user.style.color = "red";
+    }
+})
+
+//Xử lý sự kiện và biểu thức chính quy của input passWord
+function checkPass(password) {
+    var check = /^[A-Za-z0-9]{8,20}$/;
+    return check.test(password);
+}
+pass.addEventListener("blur", function () {
+    if (pass.value.length === 0) {
+        note__pass.textContent = "Must enter input";
+    }
+})
+
+pass.addEventListener("change", function () {
+    if (checkPass(pass.value)) {
+        note__pass.textContent = "";
+    }
+    else {
+        note__pass.textContent = "Must be alphabetic or numeric and length 8-20";
+        note__pass.style.color = "red";
     }
 })
