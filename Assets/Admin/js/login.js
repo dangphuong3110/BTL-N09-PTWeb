b591a6e006845893e7092e3dce5fc3c83fd74a1a
QThai1704 <quangthai170402@gmail.com>
2022-06-20 15:51:35 +0700

    chỉnh sửa, căn chỉnh thêm cho bài tập lớn

--- Assets/Admin/js/login.js
+++ Assets/Admin/js/login.js
@@ -87,6 +87,8 @@ login.addEventListener("click", function () {
     }
 })
 
+
+//create account
 //Biểu thức chính quy cho email
 var checkEmail = /^([a-zA-Z0-9]+)@gmail\.com$/;
 var loginEmail = document.getElementById('login-email');
@@ -115,20 +117,17 @@ loginSubmit.addEventListener("click", function () {
 })
 
 /* Js Create account */
-var createId = document.getElementById('create-id');
 var createEmail = document.getElementById('create-email');
 var createUser = document.getElementById('create-user');
 var createPass = document.getElementById('create-pass');
 var createComfirm = document.getElementById('create-comfirm');
 
-var noteId = document.getElementById('note-id');
-var noteEmail = document.getElementById('note-email');
-var noteUser = document.getElementById('note-user');
-var notePass = document.getElementById('note-pass');
-var noteComfirm = document.getElementById('note-comfirm');
+var noteEmail = document.getElementById('note-cr-email');
+var noteUser = document.getElementById('note-cr-user');
+var notePass = document.getElementById('note-cr-pass');
+var noteComfirm = document.getElementById('note-cr-comfirm');
 
 //Biểu thức chính quy
-var checkCreateId = /^[nv0-9]{8}$/;
 var checkCreateEmail = /^([a-zA-Z0-9]+)@gmail\.com$/;
 var checkCreateUser = /^[A-Za-z0-9]{8,20}$/;
 var checkCreatePass = /^[A-Za-z0-9]{8,20}$/;
@@ -136,24 +135,7 @@ var checkCreateComfirm = /^[A-Za-z0-9]{8,20}$/;
 
 //Xử lý sự kiện
 var btnSuccess = document.getElementById('btn-success');
-createId.addEventListener('change', function () {
-    if (checkId.test(createId.value)) {
-        noteId.textContent = "";
-    }
-    else {
-        noteId.textContent = "Must have beginning 'nv' and length 8";
-        noteId.style.color = "red";
-    }
-})
-createEmail.addEventListener('change', function () {
-    if (checkCreateEmail.test(createEmail.value)) {
-        noteEmail.textContent = "";
-    }
-    else {
-        noteEmail.textContent = "Must have the string '@gmail.com' at the end";
-        noteEmail.style.color = "red";
-    }
-})
+
 createUser.addEventListener('change', function () {
     if (checkCreateUser.test(createUser.value)) {
         noteUser.textContent = "";
@@ -182,12 +164,21 @@ createComfirm.addEventListener('change', function () {
         noteComfirm.style.color = "red";
     }
 })
+createEmail.addEventListener('change', function () {
+    if (checkCreateEmail.test(createEmail.value)) {
+        noteEmail.textContent = "";
+    }
+    else {
+        noteEmail.textContent = "Must have the string '@gmail.com' at the end";
+        noteEmail.style.color = "red";
+    }
+})
 btnSuccess.addEventListener("click", function () {
     if (createPass.value !== createComfirm.value) {
         noteComfirm.textContent = "No match"
         noteComfirm.style.color = "yellow";
     }
-    else if (checkId.test(createId.value) === false || checkEmail.test(createEmail.value) === false || checkUser.test(createUser.value) === false || checkPass.test(createPass.value) === false) {
+    else if (checkCreateEmail.test(createEmail.value) === false || checkCreateUser.test(createUser.value) === false || checkCreatePass.test(createPass.value) === false || checkCreateComfirm.test(createComfirm.value) === false) {
         alert('Unsuccess!');
     }
     else {
