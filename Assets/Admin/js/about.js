0792689eac451297991baf73f1c47052bf3fda66
Nguyễn Đăng Phương <104370972+dangphuong3110@users.noreply.github.com>
2022-06-22 14:04:15 +0700

    thêm js cho about.html, skill.html và portfolio.html

---
+++ Assets/Admin/js/about.js
@@ -0,0 +1,114 @@
+// ABOUT:
+//Add about:
+let tableAbout = document.querySelector('.table');
+let addKeyAbout = document.querySelector('.add-key-about');
+let addInformationAbout = document.querySelector('.add-information-about');
+let btnAddAbout = document.querySelector('.btn-add-about');
+var index2 = 8;
+btnAddAbout.addEventListener('click', function(){
+    if(addKeyAbout.value != "" && addInformationAbout.value != ""){
+        btnAddAbout.setAttribute('data-dismiss', 'modal');
+        var row = tableAbout.insertRow(-1);
+        var cellKey = row.insertCell(0);
+        var cellInformation = row.insertCell(1);
+        var cellED = row.insertCell(2);
+        cellKey.innerHTML = addKeyAbout.value;
+        cellInformation.innerHTML = addInformationAbout.value;
+        cellED.innerHTML = `<div class="text-center">
+                                <button type="button" class="btn btn-info btn-edit-about" data-bs-toggle="modal" data-bs-target="#exampleModal`+index2+`">
+                                    Edit
+                                </button>
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop`+index2+`">
+                                    Del
+                                </button>   
+
+                                <div class="modal fade" id="exampleModal`+index2+`" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Edit About</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                        <form>
+                                            <div class="mb-3">
+                                            <label class="form-label">Key:</label>
+                                            <input type="text" class="form-control input-key-about">
+                                            </div>
+                                        </form>
+                                        <form>
+                                            <div class="mb-3">
+                                            <label class="form-label">Information:</label>
+                                            <input type="text" class="form-control input-information-about">
+                                            </div>
+                                        </form>
+                                        </div>
+                                        <div class="modal-footer">
+                                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-info btn-save-about" data-bs-dismiss="modal">Save changes</button>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div>
+
+                                <div class="modal fade" id="staticBackdrop`+index2+`" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                        <h5 class="modal-title" id="staticBackdropLabel">Delete About</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <h5 class="text-body">Are you sure want to delete this key ?</h5>
+                                        </div>
+                                        <div class="modal-footer">
+                                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-info btn-delete-about" data-bs-dismiss="modal">Delete</button>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>`;
+        ++index2;
+        addKeyAbout.value = "";
+        addInformationAbout.value = "";
+    }
+    else{
+        btnAddAbout.setAttribute('data-dismiss', '');
+        alert("Please fill out field");
+    }
+});
+
+// Edit about:
+let btnEditAbout = document.querySelectorAll('.btn-edit-about');
+let inputKeyAbout = document.querySelectorAll('.input-key-about');
+let inputInformationAbout = document.querySelectorAll('.input-information-about');
+let btnSaveAbout = document.querySelectorAll('.btn-save-about');
+let keyAbout = document.querySelectorAll('.key-about');
+let informationAbout = document.querySelectorAll('.information-about');
+
+for(let i=0; i<keyAbout.length; i++){
+    btnEditAbout[i].addEventListener('click', function(){
+        inputKeyAbout[i].value = keyAbout[i].textContent;
+        inputInformationAbout[i].value = informationAbout[i].textContent;
+    });
+    btnSaveAbout[i].addEventListener('click', function(){
+        keyAbout[i].textContent = inputKeyAbout[i].value;
+        informationAbout[i].textContent = inputInformationAbout[i].value;
+    });
+};
+
+//Delete about:
+let btnDeleteAbout = document.querySelectorAll('.btn-delete-about');
+
+for(let i=0; i<keyAbout.length; i++){
+    btnDeleteAbout[i].addEventListener('click', function(){
+        var index;
+        for(let j=0; j<tableAbout.rows.length; j++)
+            if(tableAbout.rows[j].cells[0].textContent == keyAbout[i].textContent){
+                index = j;
+                break;
+            }
+        tableAbout.deleteRow(index);
+    });
+};
---
+++ Assets/Admin/js/skills.js
@@ -0,0 +1,115 @@
+// SKILLS:
+//Add skill:
+let tableSkill = document.querySelector('.table');
+let addNameSkill = document.querySelector('.add-name-skill');
+let addLevelSkill = document.querySelector('.add-level-skill');
+let btnAddSkill = document.querySelector('.btn-add-skill');
+var index2 = 7;
+
+btnAddSkill.addEventListener('click', function(){
+    if(addNameSkill.value != "" && addLevelSkill.value != ""){
+        btnAddSkill.setAttribute('data-dismiss', 'modal');
+        var row = tableSkill.insertRow(-1);
+        var cellName = row.insertCell(0);
+        var cellLevel = row.insertCell(1);
+        var cellED = row.insertCell(2);
+        cellName.innerHTML = addNameSkill.value;
+        cellLevel.innerHTML = addLevelSkill.value;
+        cellED.innerHTML = `<div class="text-center">
+        <button type="button" class="btn btn-info btn-edit-skill" data-bs-toggle="modal" data-bs-target="#exampleModal`+index2+`">
+          Edit
+        </button>
+        <button type="button" class="btn btn-danger btn-delete-skill" data-bs-toggle="modal" data-bs-target="#staticBackdrop`+index2+`">
+          Del
+        </button>   
+
+        <div class="modal fade" id="exampleModal`+index2+`" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Edit Skill</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+               </div>
+      <div class="modal-body">
+        <form>
+          <div class="mb-3">
+            <label class="form-label">Name:</label>
+            <input type="text" class="form-control">
+          </div>
+        </form>
+        <form>
+            <div class="mb-3">
+              <label class="form-label">Level:</label>
+              <input type="text" class="form-control">
+            </div>
+          </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-info" data-bs-dismiss="modal">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="staticBackdrop`+index2+`" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Delete Skill</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+<div class="modal-body">
+    <h5 class="text-body">Are you sure want to delete this skill ?</h5>
+</div>
+<div class="modal-footer">
+  <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+  <button type="button" class="btn btn-info" data-bs-dismiss="modal">Delete</button>
+</div>
+    </div>
+  </div>
+</div>
+    </div>`;
+        ++index2;
+        addNameSkill.value = "";
+        addLevelSkill.value = "";
+    }
+    else{
+        btnAddSkill.setAttribute('data-dismiss', '');
+        alert("Please fill out field");
+    }
+});
+
+//Edit skill:
+let btnEditSkill = document.querySelectorAll('.btn-edit-skill');
+let inputNameSkill = document.querySelectorAll('.input-name-skill');
+let inputLevelSkill = document.querySelectorAll('.input-level-skill');
+let btnSaveSkill = document.querySelectorAll('.btn-save-skill');
+let nameSkill = document.querySelectorAll('.name-skill');
+let levelSkill = document.querySelectorAll('.level-skill');
+
+for(let i=0; i<nameSkill.length; i++){
+    btnEditSkill[i].addEventListener('click', function(){
+        inputNameSkill[i].value = nameSkill[i].textContent;
+        inputLevelSkill[i].value = levelSkill[i].textContent;
+    });
+    btnSaveSkill[i].addEventListener('click', function(){
+        nameSkill[i].textContent = inputNameSkill[i].value;
+        levelSkill[i].textContent = inputLevelSkill[i].value;
+    });
+};
+
+//Delete skill:
+let btnDeleteSkill = document.querySelectorAll('.btn-delete-skill');
+
+for(let i=0; i<nameSkill.length; i++){
+    btnDeleteSkill[i].addEventListener('click', function(){
+        var index;
+        for(let j=0; j<tableSkill.rows.length; j++)
+            if(tableSkill.rows[j].cells[0].textContent == nameSkill[i].textContent){
+                index = j;
+                break;
+            }
+        tableSkill.deleteRow(index);
+    });
+};
