0418240af5996b85da735e727d038200e79277b1
QThai1704 <quangthai170402@gmail.com>
2022-06-24 10:42:03 +0700

    Bỏ tạo tài khoản

--- Assets/Admin/js/login.js
+++ Assets/Admin/js/login.js
@@ -38,13 +38,7 @@ loginBack.addEventListener("click", function () {
     loginPassword.style.display = 'none';
     loginContact.style.display = 'block';
 })
-loginAccount.addEventListener("click", function () {
-    createUser.value = "";
-    createPass.value = "";
-    createEmail.value = "";
-    createId.value = "";
-    createComfirm.value = "";
-})
+
 
 
 var user = document.getElementById('userName');
@@ -87,20 +81,18 @@ login.addEventListener("click", function () {
     }
 })
 
-
-//create account
 //Biểu thức chính quy cho email
 var checkEmail = /^([a-zA-Z0-9]+)@gmail\.com$/;
 var loginEmail = document.getElementById('login-email');
 var loginSubmit = document.getElementById('login-submit');
-var noteEmail = document.getElementById('note-email');
+var noteLoginEmail = document.getElementById('note-email');
 loginEmail.addEventListener("change", function () {
     if (checkEmail.test(loginEmail.value)) {
         noteLoginEmail.textContent = "";
         loginSubmit.classList.remove('disabled');
     }
     else {
-        noteEmail.textContent = "Must have the string '@gmail.com' at the end";
+        noteLoginEmail.textContent = "Must have the string '@gmail.com' at the end";
         loginSubmit.classList.add('disabled');
         noteLoginEmail.style.color = 'red';
         noteLoginEmail.style.fontSize = '20px';
@@ -116,75 +108,7 @@ loginSubmit.addEventListener("click", function () {
     }
 })
 
-/* Js Create account */
-var createEmail = document.getElementById('create-email');
-var createUser = document.getElementById('create-user');
-var createPass = document.getElementById('create-pass');
-var createComfirm = document.getElementById('create-comfirm');
 
-var noteEmail = document.getElementById('note-cr-email');
-var noteUser = document.getElementById('note-cr-user');
-var notePass = document.getElementById('note-cr-pass');
-var noteComfirm = document.getElementById('note-cr-comfirm');
-
-//Biểu thức chính quy
-var checkCreateEmail = /^([a-zA-Z0-9]+)@gmail\.com$/;
-var checkCreateUser = /^[A-Za-z0-9]{8,20}$/;
-var checkCreatePass = /^[A-Za-z0-9]{8,20}$/;
-var checkCreateComfirm = /^[A-Za-z0-9]{8,20}$/;
-
-//Xử lý sự kiện
-var btnSuccess = document.getElementById('btn-success');
-
-createUser.addEventListener('change', function () {
-    if (checkCreateUser.test(createUser.value)) {
-        noteUser.textContent = "";
-    }
-    else {
-        noteUser.textContent = "Must be alphabetic or numeric and length 8-20";
-        noteUser.style.color = "red";
-
-    }
-})
-createPass.addEventListener('change', function () {
-    if (checkCreatePass.test(createPass.value)) {
-        notePass.textContent = "";
-    }
-    else {
-        notePass.textContent = "Must be alphabetic or numeric and length 8-20";
-        notePass.style.color = "red";
-    }
-})
-createComfirm.addEventListener('change', function () {
-    if (checkCreateComfirm.test(createComfirm.value)) {
-        noteComfirm.textContent = "";
-    }
-    else {
-        noteComfirm.textContent = "Must be alphabetic or numeric and length 8-20";
-        noteComfirm.style.color = "red";
-    }
-})
-createEmail.addEventListener('change', function () {
-    if (checkCreateEmail.test(createEmail.value)) {
-        noteEmail.textContent = "";
-    }
-    else {
-        noteEmail.textContent = "Must have the string '@gmail.com' at the end";
-        noteEmail.style.color = "red";
-    }
-})
-btnSuccess.addEventListener("click", function () {
-    if (createPass.value !== createComfirm.value) {
-        noteComfirm.textContent = "No match"
-        noteComfirm.style.color = "yellow";
-    }
-    else if (checkCreateEmail.test(createEmail.value) === false || checkCreateUser.test(createUser.value) === false || checkCreatePass.test(createPass.value) === false || checkCreateComfirm.test(createComfirm.value) === false) {
-        alert('Unsuccess!');
-    }
-    else {
-        alert('Success!');
-    }
-})
 
 
 
